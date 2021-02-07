3e7886365fe2acc8897f5765e59b859b1b39ac96
Ben Imberman <bimberman@gmail.com>
2021-02-06 22:12:02 -0800

    pramp-decrypt

---
+++ pramp/decrypt.js
@@ -0,0 +1,17 @@
+function decrypt(word) {
+  // your code goes here
+  let res = "";
+  let prevChar = 1;
+
+  for (let i = 0; i < word.length; i++) {
+    let char = word.charCodeAt(i) - prevChar;
+    if (i !== 0) {
+      while (char < 97) {
+        char += 26;
+      }
+    }
+    prevChar += char;
+    res += String.fromCharCode(char);
+  }
+  return res;
+}
